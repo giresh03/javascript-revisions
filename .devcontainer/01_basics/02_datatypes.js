7a49b84b0fec4104ea9b1df4deacaae9f60eeadb
giresh03 <135005276+giresh03@users.noreply.github.com>
2024-12-13 13:58:31 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name ="Girish"
+let age = 18
+let IsLoggedIn = false
+let state
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Girish");
